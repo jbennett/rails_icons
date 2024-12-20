111ef82dd70ba9e0d846e9091f98fac1f03a9dff
e <988051+eelcoj@users.noreply.github.com>
2024-12-20 20:18:40 +0000

    Use custom destination  (#44)
    * Use configuration option for destiation path
    
    * Use given destination on install/initialize generator

--- lib/generators/rails_icons/sync_generator.rb
+++ lib/generators/rails_icons/sync_generator.rb
@@ -4,11 +4,11 @@ require "rails_icons/sync/engine"
 
 module RailsIcons
   class SyncGenerator < Rails::Generators::Base
-    class_option :libraries, type: :array, default: [], desc: "Choose libraries (#{RailsIcons.libraries.keys.join("/")})"
-    class_option :destination, type: :string, default: "app/assets/svg/icons/", desc: "Specify destination folder for icons"
-
-    desc "Sync an icon library(s) from their respective git repos."
     source_root File.expand_path("templates", __dir__)
+
+    desc "Sync the choosen icon libraries from their respective git repos."
+
+    class_option :libraries, type: :array, default: [], desc: "Choose libraries (#{RailsIcons.libraries.keys.join("/")})"
 
     def sync_icons
       raise "[Rails Icons] Not a valid library" if options[:libraries].empty?
